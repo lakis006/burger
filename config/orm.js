dfd8a1e129c1e3f02fce839ae32482d8e67b43ff
Jsquad <lakis006@umn.edu>
2020-06-29 21:14:13 -0500

    populated the orm.js with connections and functions that will insert, update, and select all burgers

---
+++ config/orm.js
@@ -0,0 +1,37 @@
+var connection = require("/connection.js");
+
+var tableName = "burgers";
+
+var orm = {
+    selectAll: function(callback) {
+        var s = "SELECT * FROM" +  tableName;
+
+        connection.query(s, function(err, result) {
+            callback(result);
+        });
+    },
+
+
+
+    insertOne: function(insert, callback) {
+        var s = "INSERT INTO " + tableName + " (text, complete) VALUES (?,?)";
+        insert.complete = insert.complete || 0;
+        connection.query(s, [insert.text, insert.complete], function(err, result) {
+            callback(result)
+        });
+    },
+
+
+    updateOne: function(insert, callback) {
+        var s = "UPDATE " + tableName + " SET text=? WHERE id=?";
+
+        connection.query(s, [
+            insert.text, insert.id
+        ], function(err, result) {
+            callback(result)
+        });
+    }
+
+};
+
+module.exports = orm;
