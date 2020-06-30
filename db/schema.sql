4fa934bc2373589987138a4dc3e28afa50f89958
Jsquad <lakis006@umn.edu>
2020-06-29 20:20:06 -0500

    added my sql schema and seed files

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,12 @@
-DROP DATABASE IF EXISTS burgers
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    burger_name VARCHAR(255), 
+    devours BOOLEAN
+    );
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES
+("Sloppy Joe", FALSE),
+("Juicey Lucy", TRUE),
+("Tofu", FALSE),
+("Steak", TRUE);
