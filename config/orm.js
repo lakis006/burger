fb565ac5ef14ce7c8e796b9d33bf56c5e05b62ff
Jsquad <lakis006@umn.edu>
2020-06-30 21:50:24 -0500

    added the right callback to cb

--- config/orm.js
+++ config/orm.js
@@ -3,32 +3,32 @@ var connection = require("./connection.js");
 var tableName = "burgers";
 
 var orm = {
-    selectAll: function(callback) {
+    selectAll: function(cb) {
         var s = "SELECT * FROM" +  tableName;
 
         connection.query(s, function(err, result) {
-            callback(result);
+            cb(result);
         });
     },
 
 
 
-    insertOne: function(insert, callback) {
+    insertOne: function(insert, cb) {
         var s = "INSERT INTO " + tableName + " (text, complete) VALUES (?,?)";
         insert.complete = insert.complete || 0;
         connection.query(s, [insert.text, insert.complete], function(err, result) {
-            callback(result)
+            cb(result)
         });
     },
 
 
-    updateOne: function(insert, callback) {
+    updateOne: function(insert, cb) {
         var s = "UPDATE " + tableName + " SET text=? WHERE id=?";
 
         connection.query(s, [
             insert.text, insert.id
         ], function(err, result) {
-            callback(result)
+            cb(result)
         });
     }
 
