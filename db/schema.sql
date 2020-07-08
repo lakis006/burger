fbcc656447e60dd0a195d2583817341f2995fe51
Jsquad <lakis006@umn.edu>
2020-07-07 22:39:55 -0500

    added the start server.js to the package.json

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+
 CREATE DATABASE burgers_db;
 USE burgers_db;
-CREATE TABLE burgers
-	 (id INT AUTO INCREMENT NOT_NULL,
-	  burger_name VARCHAR(20),
-	  devoured BOOLEAN, 
-	  date TIMESTAMP
-	  PRIMARY KEY (id)
-);
 
-CREATE TABLE `burgers_db`.`burgers` (
-  `id` INT UNSIGNED NOT NULL,
-  `burger_name` VARCHAR(45) NOT NULL,
-  `devoured` BOOLEAN NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `devoured_UNIQUE` (`devoured` ASC) VISIBLE);
+
+
+CREATE TABLE burgers (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  burger_name VARCHAR(45) NOT NULL,
+  devoured BOOLEAN NULL
+  );
