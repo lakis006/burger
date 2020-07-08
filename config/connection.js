251c8049858c9ac157b1d3f259526b2410049a19
Jsquad <lakis006@umn.edu>
2020-07-07 23:10:19 -0500

    removed const from connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 if (process.env.JAWSDB_URL) {
- const connection = mysql.createConnection(process.env.JAWSDB_URL) 
+  connection = mysql.createConnection(process.env.JAWSDB_URL) 
 } else {
 	connection = mysql.createConnection({
 		host: "localhost",
