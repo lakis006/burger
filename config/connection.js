b5c56668f8375345219c114cbc5d2218714529e3
Jsquad <lakis006@umn.edu>
2020-07-07 22:58:56 -0500

    added the JAWSDB in hopes that it'll run through github

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require('mysql');
- const connection = mysql.createConnection ({
-	host: "localhost",
-	port: 3306,
- 	user: "root",
- 	password: "#Jsquadisgr8",
-	database: "burgers_db",
-	multipleStatements: true 
- });
+
+if (process.env.JAWSDB_URL) {
+ const connection = mysql.createConnection(process.env.JAWSDB_URL) 
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		 user: "root",
+		 password: "#Jsquadisgr8",
+		database: "burgers_db",
+		multipleStatements: true 
+	});
+ };
  
  connection.connect(function(err) {
  	if (err) {
